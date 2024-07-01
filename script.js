6f6dbe2cb80c1264a125d63603db583a0bd80d17
Quinten Konyn <qkonyn@gmail.com>
2024-07-01 15:57:30 -0700

    Add displayLibrary function

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ function Book () {
 function addBookToLibrary(input) {
 	myLibrary.push(new Book(input));
 }
+
+function displayLibrary() {
+	let library = myLibrary;
+	let libraryElement = document.getElementById('library');
+	library.forEach((book) => {
+		let bookElement = document.createElement('li');
+		bookElement.textContent = `${book.title} by ${book.author}`;
+		libraryElement.append(bookElement);
+	});
+}
+
+myLibrary.push({title: "The Hobbit", author: "Tolkein"});
+myLibrary.push({title: "So You Want To Be A Wizard", author: "Diane Duane"});
+myLibrary.push({title: "Going Postal", author: "Terry Pratchett"});
+
+displayLibrary();
