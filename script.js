04459bdd5d604232b8dd492234c4eea777204437
Quinten Konyn <qkonyn@gmail.com>
2024-07-02 21:23:08 -0700

    Allow book submission to library with form

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 const myLibrary = [];
 
-function Book () {
-	// the constructor...
+function Book (title, author) {
+	this.title = title;
+	this.author = author;
 }
 
-function addBookToLibrary(input) {
-	myLibrary.push(new Book(input));
+// TODO: don't allow empty books to be submitted (validate first)
+function submitNewBook(event) {
+	event.preventDefault();
+	var newBook = addBookToLibrary(this[0].value, this[1].value);
+	this.reset();
+	displayLibrary();
+}
+
+function addBookToLibrary(title, author) {
+	var newBook = new Book(title, author);
+	myLibrary.push(newBook);
+	return newBook;
 }
 
 function displayLibrary() {
 	let library = myLibrary;
 	let libraryElement = document.getElementById('library');
+	// TODO: don't remove then add all children each time we modify the book list
+	while (libraryElement.firstChild) {
+		libraryElement.removeChild(libraryElement.firstChild);
+	}
 	library.forEach((book) => {
 		let bookElement = document.createElement('li');
 		bookElement.textContent = `${book.title} by ${book.author}`;
@@ -18,8 +33,12 @@ function displayLibrary() {
 	});
 }
 
-myLibrary.push({title: "The Hobbit", author: "Tolkein"});
-myLibrary.push({title: "So You Want To Be A Wizard", author: "Diane Duane"});
-myLibrary.push({title: "Going Postal", author: "Terry Pratchett"});
+const addBookForm = document.getElementById('add-book');
+addBookForm.addEventListener("submit", submitNewBook);
+
+addBookToLibrary("The Hobbit", "Tolkein");
+addBookToLibrary("So You Want To Be A Wizard", "Diane Duane");
+addBookToLibrary("Going Postal", "Terry Pratchett");
 
 displayLibrary();
+
