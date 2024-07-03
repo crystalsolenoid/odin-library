20045cff49efbca0fa8a14c4af1b9c2099410b12
Quinten Konyn <qkonyn@gmail.com>
2024-07-02 22:57:00 -0700

    Add read status and button to toggle it

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 const myLibrary = [];
 
-function Book (title, author) {
+function Book (title, author, read) {
 	this.title = title;
 	this.author = author;
+	this.read = read;
 
-	[this.element, this.deleteButton] = this.generateElement();
+	[this.element, this.deleteButton, this.readElement] = this.generateElement();
 }
 
 Book.prototype.generateElement = function() {
 	let bookElement = document.createElement('li');
 	bookElement.textContent = `${this.title} by ${this.author}`;
+
+	let readElement = document.createElement('button');
+	readElement.textContent = this.read ? "read" : "not read";
+	readElement.addEventListener("click", toggleRead);
+	readElement.book = this;
+	bookElement.append(readElement);
+
 	deleteButton = document.createElement('button');
 	deleteButton.textContent = "delete";
 	bookElement.append(deleteButton);
 	deleteButton.addEventListener("click", deleteBook);
-	return [bookElement, deleteButton];
+
+	return [bookElement, deleteButton, readElement];
 };
 
 Book.prototype.setIndex = function(index) {
@@ -40,15 +49,21 @@ function deleteBook(event) {
 	book.deleteFromLibrary()
 }
 
+function toggleRead(event) {
+	this.book.read = !this.book.read;
+	this.textContent = this.book.read ? "read" : "not read";
+}
+
+
 // TODO: don't allow empty books to be submitted (validate first)
 function submitNewBook(event) {
 	event.preventDefault();
-	var newBook = addBookToLibrary(this[0].value, this[1].value);
+	var newBook = addBookToLibrary(this[0].value, this[1].value, this[2].checked);
 	this.reset();
 }
 
-function addBookToLibrary(title, author) {
-	var newBook = new Book(title, author);
+function addBookToLibrary(title, author, read) {
+	var newBook = new Book(title, author, read);
 	newBook.addToLibrary(myLibrary);
 	return newBook;
 }
@@ -56,6 +71,6 @@ function addBookToLibrary(title, author) {
 const addBookForm = document.getElementById('add-book');
 addBookForm.addEventListener("submit", submitNewBook);
 
-addBookToLibrary("The Hobbit", "Tolkein");
-addBookToLibrary("So You Want To Be A Wizard", "Diane Duane");
-addBookToLibrary("Going Postal", "Terry Pratchett");
+addBookToLibrary("The Hobbit", "Tolkein", true);
+addBookToLibrary("So You Want To Be A Wizard", "Diane Duane", true);
+addBookToLibrary("Going Postal", "Terry Pratchett", false);
